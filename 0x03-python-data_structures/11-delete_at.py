daa9de34c39e2e8d6b4892d76c3ce705f436263f
Wisdom Sena <wisdomsena36@gmail.com>
2023-09-10 09:40:22 -0700

    Fix betty style for 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,7 +2,7 @@
 def delete_at(my_list=[], idx=0):
     list_len = len(my_list) - 1
     if idx < 0 or idx > list_len:
-        return(my_list)
+        return (my_list)
     else:
         del my_list[idx]
-        return(my_list)
+        return (my_list)
