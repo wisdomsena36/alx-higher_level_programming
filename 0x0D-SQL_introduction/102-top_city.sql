ad639cbc72f62ccecb5ac72eda8397e02cb44287
Wisdom Sena <wisdomsena36@gmail.com>
2023-11-15 06:52:16 -0800

    Add script for 102-top_city.sql

---
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -0,0 +1,7 @@
+-- Temp #1
+-- display top 3 city, avg(temp) July to August
+SELECT city, AVG(value) AS "avg_temp" FROM temperatures
+WHERE month = 7 OR month = 8
+GROUP BY city
+ORDER BY avg_temp DESC
+LIMIT 3;
