b99f241f34e63869c4a03a6938bdbde9c6cfdcec
Wisdom Sena <wisdomsena36@gmail.com>
2023-11-15 06:03:40 -0800

    Add script for 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,17 @@
+-- Creates first table
+-- Create second table
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT,
+    name VARCHAR(256),
+    score INT
+) ENGINE=INNODB;
+-- Insert rows
+INSERT INTO second_table (
+	id, 
+	name,
+	score
+)
+VALUES (1, "John", 10),
+       (2, "Alex", 3),
+       (3, "Bob", 14),
+       (4, "George", 8);
