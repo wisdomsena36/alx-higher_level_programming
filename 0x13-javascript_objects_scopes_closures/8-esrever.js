c536b292b17241b079616d5fbba8fb1923318c64
Wisdom Sena <wisdomsena36@gmail.com>
2023-12-13 02:02:28 -0800

    Fix 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,7 +1,5 @@
+#!/usr/bin/node
+// reverses a list
 exports.esrever = function (list) {
-  const reversed = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    reversed.push(list[i]);
-  }
-  return reversed;
+  return list.sort(() => -1);
 };
