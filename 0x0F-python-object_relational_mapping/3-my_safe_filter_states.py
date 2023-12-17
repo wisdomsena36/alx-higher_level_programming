d81a445f19a5a8f2b589503aad6adf6b4a7805c7
Wisdom Sena <wisdomsena36@gmail.com>
2023-12-17 07:39:22 -0800

    Add 3-my_safe_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Lists all states starting with passed arg, prevents injection"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    connection = MySQLdb.connect(host="localhost", port=3306, charset="utf8",
+                                 user=argv[1], passwd=argv[2], db=argv[3])
+    cursor = connection.cursor()
+    cursor.execute(
+                   "SELECT * FROM states WHERE name = %s ORDER BY states.id ASC",
+        (argv[4], ))
+    query_rows = cursor.fetchall()
+    for row in query_rows:
+        print(row)
+    cursor.close()
+    connection.close()
